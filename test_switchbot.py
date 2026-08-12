5932460b3d9115c8975a55ee333f86aae548a51a
Kyle C <Kjcalip1@gmail.com>
2026-08-11 20:24:31 -0700

    SwitchBot API Test
    Able to connect with credentials and verify success code 200.  Runs as is.

---
+++ test_switchbot.py
@@ -0,0 +1,47 @@
+import os
+import time
+import uuid
+import hmac
+import hashlib
+import base64
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+token = os.getenv("SWITCHBOT_TOKEN")
+secret = os.getenv("SWITCHBOT_SECRET")
+
+nonce = str(uuid.uuid4()) # Get Random identifier for request
+timestamp = str(int(time.time() * 1000)) # Required for SwitchBot (13-digit timestamp in ms)
+
+string_to_sign = token + timestamp + nonce
+
+# Cryptographic Signature
+signature = base64.b64encode(
+    hmac.new(
+        secret.encode("utf-8"),
+        string_to_sign.encode("utf-8"),
+        hashlib.sha256
+    ).digest()
+).decode("utf-8")
+
+headers = {
+    "Authorization": token,
+    "sign": signature,
+    "nonce": nonce,
+    "t": timestamp,
+}
+
+# Actual internet communication
+response = requests.get(
+    "https://api.switch-bot.com/v1.1/devices",
+    headers=headers
+)
+
+print(response.status_code)
+data = response.json()
+
+print(data)
